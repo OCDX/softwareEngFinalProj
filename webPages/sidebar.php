239752868c3a06e8c1b59786f64bc8e850c197a1
Zach Dolan <zmd989@mail.missouri.edu>
2016-10-31 11:43:53 -0500

    Update sidebar.php
    account for https

--- webPages/sidebar.php
+++ webPages/sidebar.php
@@ -4,8 +4,8 @@
 				<input type="text" name="search" id="search">
 				
 			</form>
-			<a href="http://mizzou.tech/userInfo.php"><h4>Your Account</h4></a>
-			<a href="http://mizzou.tech/create-edit.php"><h4>Manifest Editor</h4></a>
+			<a href="https://mizzou.tech/userInfo.php"><h4>Your Account</h4></a>
+			<a href="https://mizzou.tech/create-edit.php"><h4>Manifest Editor</h4></a>
 			<button id="logout" class="button">Log Out</button>
 		</div>
 	</div>
