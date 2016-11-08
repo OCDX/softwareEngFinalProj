419fbca27699cdd825d9aa293ef14a1b5091ee9a
erbmt9 <erbmt9@mail.missouri.edu>
2016-11-08 15:11:46 -0600

    removing things and adding things

--- webPages/create-edit.php
+++ webPages/create-edit.php
@@ -16,22 +16,25 @@
 		</div>
 		<div class="viewer column two-thirds">
 			<p>Taking user input, files, get date and time. Buttons for update existing or create new.</p>
-			<button id="new" class="button" onclick="formNewFctn()">Create New</button><button id="edit" class="button">Edit this Manifest</button>
-			<p id="new"></p>
+			<button id="newBtn" class="button" onclick="formNewFctn(1)">Create New</button><button id="edit" class="button">Edit this Manifest</button>
+			<div id="new">
+				<form action="create-edit.php" method="POST">
+					<p>Title of Manifest</p>
+					<input type="text" id="title" name="title"></input>
+					<p>Script</p>
+					<input type="file" id="script" name="script"></input>
+				</form>
+			</div>
 			<p>If edit, show existing fields for maifest and associated files. Present options for uploading new files and removing old ones.
 			If new, present empty fields and upload options for each file. 
 			For both, take timestamp, set as most recent update.</p>
 			
 		<script>	
-			function formNewFctn(){
-		</script>
-				<form action='create-edit.php' method='POST'>
-					<p>Title of Manifest</p>
-					<input type='text' id='title' name='title'></input>
-					<p>Script</p>
-					<input type='file' id='script' name='script'></input>
-				</form>
-		<script>
+			function formNewFctn(i){
+				if(i == 1)
+					document.getElementById("new").style.display=block;
+				else
+					document.getElementById("new").style.display=none;
 			}
 		</script>
 		</div>	
