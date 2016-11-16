31f76920bb37d98e620e27444720f0ed63c0d17f
Zach Dolan <zmd989@mail.missouri.edu>
2016-11-16 14:43:08 -0600

    Update create-edit.php
    restrict access to non logged in users and redirect them, if logged in, display the form

--- webPages/create-edit.php
+++ webPages/create-edit.php
@@ -39,61 +39,43 @@
 	</div>
 	<div class="content column full">
 		<div class="page-head">
-			<h2>Manifest Editor</h2>
-		</div>
-		<div class="viewer column two-thirds">
-			<p>Taking user input, files, get date and time. Buttons for update existing or create new.
-			If edit, show existing fields for maifest and associated files. Present options for uploading new files and removing old ones.
-			If new, present empty fields and upload options for each file. 
-			For both, take timestamp, set as most recent update.</p>
-			<button id="newBtn" class="button" onclick="formNewFctn(1)">Create New</button><button id="edit" class="button" onclick="formEditFctn(1)">Edit Manifest</button>
-			
-			<div id="new">
-				<form action="create-edit.php" method="POST">
-					<p>Title of Manifest</p>
-					<input type="text" id="title" name="title">
-					<p>Version</p>
-					<input type="text" id="manifestVersion" name="manifestVersion">
-					<p>Description</p>
-					<input type="text" id="description" name="description">
-					<p>Category</p>
-					<input type="text" id="category" name="category">
-					<p>Keywords</p>
-					<input type="text" id="keyword" name="keyword">
-					<p>Script</p>
-					<input type="file" id="script" name="script">
-										
-					<br>
-					<input type="submit" value="Submit">
-				</form>
-				<?php 
-					//take fields and timestamp then load into tables
-				?>
-			</div>
-			
-			<!-- <div id="edit">
-				<form action="create-edit.php" method="POST">
-					<p>Title of Manifest</p>
-					<input type="text" id="title" name="title">
-					<p>Version</p>
-					<input type="text" id="manifestVersion" name="manifestVersion">
-					<p>Description</p>
-					<input type="text" id="description" name="description">
-					<p>Category</p>
-					<input type="text" id="category" name="category">
-					<p>Keywords</p>
-					<input type="text" id="keyword" name="keyword">
-					<p>Script</p>
-					<input type="file" id="script" name="script">
-										
-					<br>
-					<input type="submit" value="Submit">
-				</form>
-				<?php 
-					//take fields and timestamp then load into tables
-				?>
-			</div> -->
-			
-		</div>	
-	</div>
+<?php if ($_SESSION['email'] == NULL){
+                        echo "<h2> You must be signed into view this content</h2>";
+                        header('Refresh: 3; URL=index.php');
+                        }
+else {
+        echo"
+                        <h2>Manifest Editor</h2>
+                </div>
+                <div class='viewer column two-thirds'>
+                        <p>Taking user input, files, get date and time. Buttons for update existing or create new.
+                        If edit, show existing fields for maifest and associated files. Present options for uploading new files and removing old ones.
+                        If new, present empty fields and upload options for each file. 
+                        For both, take timestamp, set as most recent update.</p>
+                        <button id='newBtn' class='button' onclick='formNewFctn(1)'>Create New</button><button id='edit' class='button' onclick='formEditFctn(1)'>Edit Manifest</button>
+                        
+                        <div id='new'>
+                                <form action='create-edit.php' method='POST'>
+                                        <p>Title of Manifest</p>
+                                        <input type='text' id='title' name='title'>
+                                        <p>Version</p>
+                                        <input type='text' id='manifestVersion' name='manifestVersion'>
+                                        <p>Description</p>
+                                        <input type='text' id='description' name='description'>
+                                        <p>Category</p>
+                                        <input type='text' id='category' name='category'>
+                                        <p>Keywords</p>
+                                        <input type='text' id='keyword' name='keyword'>
+                                        <p>Script</p>
+                                        <input type='file' id='script' name='script'>
+                                                                                
+                                        <br>
+                                        <input type='submit' value='Submit'>
+                                </form>
+                        </div>
+                        
+                        
+                        
+                </div>  
+        </div>";}?>
 <?php include_once("footer.php"); ?>
