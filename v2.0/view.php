5f2e7d6f21dabf84fd0b7108b380fc1e58d1cc5f
Unknown <jared.welch1@gmail.com>
2016-11-27 23:04:28 -0600

    fix view

--- v2.0/view.php
+++ v2.0/view.php
@@ -10,18 +10,31 @@
   		
 		</div>
 	</div>
-			<p>Display all files as a table that are associated with manifest.</p>
-	
+
 <?php 	if ($_SESSION['email'] == NULL){
 		echo "<h1> Error you must be signed in to view content</h1>";
 		echo "<meta http-equiv='refresh' content='0;url=index.php'>";
-	}
-	else{
-	$conn = mysqli_connect('localhost','admin','CS4320FG7','SEFinalProject') or die ("error connecting to database");
+	} else {
+	
+		$conn = mysqli_connect('localhost','admin','CS4320FG7','SEFinalProject') or die ("error connecting to database");
         $printquery = "SELECT * FROM manifest";
         $result = mysqli_query($conn, $printquery);
+        echo"<table class='table table-bordered table-hover table-striped'>
+			<tr>
+				<th>Manifest ID</th>
+				<th>Version</th>
+				<th>Category</th>
+				<th>Last Edit</th>
+				<th>Upload Date</th>
+				<th>Title</th>
+			</tr>";
         while ($row = mysqli_fetch_assoc($result)){
-        echo "<table class='table table-bordered table-hover table-striped'><tr><th>Manifest ID</th><th>Version</th><th>Category</th><th>Last Edit</th><th>Upload Date</th><th>Title</th><th>Owner ID</th><th>Content</th></tr><tr><td>".$row['manifest_id']."</td><td>".$row['version']."</td><td>".$row['category']."</td><td>".$row['last_edit']."</td><td>".$row['upload_date']."</td><td>".$row['title']."</td><td>".$row['ownerID']."</td><td>".$row['data']."</td></tr></table>";}}?>		
+        echo "<tr><td>".$row['manifest_id']."</td><td>".$row['version']."</td><td>".$row['category']."</td><td>".$row['last_edit']."</td><td>".$row['upload_date']."</td><td>".$row['title']."</td><td><a class='manifest'>View</a></td></tr>";
+        }
+        echo"</table>";
+    }
+
+?>		
 	</div>	
 	</div>
 <?php include_once("footer.php");?>
