43c10ec180e4d7f6e0535e5f7b51febeb9b448bf
Geoff <gahyvb@gmail.com>
2016-11-28 21:47:24 -0600

    updates
    view/download

---
+++ v2.0/download.php
@@ -0,0 +1,22 @@
+<?php
+$conn = mysqli_connect('localhost','admin','CS4320FG7','SEFinalProject') or die ("error connecting to database");
+
+function download(){
+          $id = $row['manifest_id'];
+          $query = "SELECT * " ."FROM manifest WHERE manifest_id = '$id'";
+          $result2 = mysqli_query($connection,$query)
+                     or die('Error, query failed');
+         list($id, $file, $type, $size,$content) =   mysqli_fetch_array($result2);
+           //echo $id . $file . $type . $size;
+         header("Content-length: $size");
+         header("Content-type: $type");
+         header("Content-Disposition: attachment; filename=$file");
+         ob_clean();
+         flush();
+         echo $content;
+         mysqli_close($connection);
+         exit;
+}
+download();
+        ?>
+
--- v2.0/view.php
+++ v2.0/view.php
@@ -21,7 +21,8 @@
         $printquery = "SELECT * FROM manifest";
         $result = mysqli_query($conn, $printquery);
         while ($row = mysqli_fetch_assoc($result)){
-        echo "<table class='table table-bordered table-hover table-striped'><tr><th>Manifest ID</th><th>Version</th><th>Category</th><th>Last Edit</th><th>Upload Date</th><th>Title</th><th>Owner ID</th><th>Content</th></tr><tr><td>".$row['manifest_id']."</td><td>".$row['version']."</td><td>".$row['category']."</td><td>".$row['last_edit']."</td><td>".$row['upload_date']."</td><td>".$row['title']."</td><td>".$row['ownerID']."</td><td>".$row['data']."</td></tr></table>";}}?>		
+        //echo "<tr><td>".$row['manifest_id']."</td><td>".$row['version']."</td><td>".$row['category']."</td><td>".$row['last_edit']."</td><td>".$row['upload_date']."</td><td>".$row['title']."</td><td><a class='manifest'>View</a></td></tr>";
+        echo "<tr><td>".$row['manifest_id']."</td><td>".$row['version']."</td><td>".$row['category']."</td><td>".$row['last_edit']."</td><td>".$row['upload_date']."</td><td>".$row['title']."</td><td><a class='manifest'>View</a></td><td><a href = 'download.php' class='manifest'>Download</a></td></tr>";
 	</div>	
 	</div>
 <?php include_once("footer.php");?>
