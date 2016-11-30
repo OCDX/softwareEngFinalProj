6055673b0a065189bda1af9febe4cb876f48813e
jaredwelch1 <jared.welch1@gmail.com>
2016-11-29 20:05:23 -0600

    did it work?

--- v2.0/view.php
+++ v2.0/view.php
@@ -21,7 +21,7 @@
         $result = mysqli_query($conn, $printquery);
         echo"<table class='table table-bordered table-hover table-striped'>
 			<tr>
-				<th>Manifest ID (PEANBUT)</th>
+				<th>Manifest ID (PEANBUTTER)</th>
 				<th>Version</th>
 				<th>Category</th>
 				<th>Last Edit</th>
