4d8fc5b7c6be90b03011a1b25b89ac0857ca8dee
jaredwelch1 <jared.welch1@gmail.com>
2016-11-29 20:15:57 -0600

    git push test!

--- v2.0/view.php
+++ v2.0/view.php
@@ -21,7 +21,7 @@
         $result = mysqli_query($conn, $printquery);
         echo"<table class='table table-bordered table-hover table-striped'>
 			<tr>
-				<th>Manifest ID (weedasdf)</th>
+				<th>Manifest ID</th>
 				<th>Version</th>
 				<th>Category</th>
 				<th>Last Edit</th>
