f6b0d34d109a373c395d27c6db022c0d22abaac8
Zach Dolan <zmd989@mail.missouri.edu>
2016-12-16 13:34:22 -0600

    Update userInfo.php

--- v2.0/userInfo.php
+++ v2.0/userInfo.php
@@ -10,31 +10,61 @@
 			get user's manifests: userManifests()
 			display manifests as table: displayManifests() -->
 
+<script type="text/javascript">
+$.extend(
+	{
+	    redirectPost: function(location, args)
+	    {
+	        var form = '';
+	        $.each( args, function( key, value ) {
+	            form += '<input type="hidden" name="'+key+'" value="'+value+'">';
+	        });
+	        $('<form action="'+location+'" method="POST">'+form+'</form>').appendTo('body').submit();
+	    }
+	});
+
+	function sendID(id) {
+		var redirect = 'create-edit.php';
+		$.redirectPost(redirect, {'id' : id});
+	}</script>
 	<div class="content column full">
 		<div class="maifestList column two-thirds">
 			<h3>Your Manifests</h3>
-			<p>Display list of manifests as a table, up to so many items. Anything beyond will be scrolled to, in a scroll container.
-			Title, date, list of files. SQL and php implementation.
-			Clicking on the title should go to the view page, where there will be options to edit if they are the author, going to the edit page.</p>
-
 			<div class="scroll-container">
 				<div class="scroll-box">
-					<?php
-						//Show user's mainfests
-						if ($_SESSION['email'] == NULL){
-                        echo "<h1>Error: You must be signed in to view this content</h1>";
-                        echo "<meta http-equiv='refresh' content='0;url=index.php'>";
-                        }
-						else {
+					<?php 	if ($_SESSION['email'] == NULL){
+		echo "<h1> Error you must be signed in to view content</h1>";
+		echo "<meta http-equiv='refresh' content='0;url=index.php'>";
+	} else {
+	
+		$conn = mysqli_connect('localhost','admin','CS4320FG7','SEFinalProject') or die ("error connecting to database");
+        $id = $_SESSION['userid'];
+        $printquery = "SELECT * FROM manifest WHERE ownerID = $id";
+        $result = mysqli_query($conn, $printquery);
+        echo"<table class='table table-bordered table-hover table-striped'>
+			<tr>
+				<th>Title</th>
+				<th>Version</th>
+				<th>Category</th>
+				<th>Last Edit</th>
+				<th>Upload Date</th>
+                <th>Edit</th>
+			</tr>";
+        while ($row = mysqli_fetch_assoc($result)){
+        echo 
+        	"<tr>
+	            <td>".$row['title']."</td>
+	            <td>".$row['version']."</td>
+	            <td>".$row['category']."</td>
+	            <td>".$row['last_edit']."</td>
+	            <td>".$row['upload_date']."</td>
+	            <td><button class='manifest' onClick='sendID(" . $row['manifest_id'].");'>View</button></td>
+            </tr>";
+        }
+        echo"</table>";
+    }
 
-						$printquery = "SELECT * FROM manifest WHERE ownerID LIKE 1";
-        		$conn = mysqli_connect('localhost','admin','CS4320FG7','SEFinalProject') or die ("error connecting to database");
-						$result = mysqli_query($conn, $printquery);
-       					while ($row = mysqli_fetch_assoc($result)){
-				        	echo "<div class='table-responsive'><table class='table table-striped table-hover table-boredered'><tr><th>Manifest ID</th><th>Version</th><th>Category</th><th>Last Edit</th><th>Upload Date</th><th>Title</th><th>Owner ID</th><th>Content</th></tr><tr><td>".$row['manifest_id']."</td><td>".$row['version']."</td><td>".$row['category']."</td><td>".$row['last_edit']."</td><td>".$row['upload_date']."</td><td>".$row['title']."</td><td>".$row['ownerID']."</td><td>".$row['data']."</td></tr></table></div";
-       					}}
-						mysqli_close($conn);
-					?>
+?>
 				</div>
 			</div>
 		</div>
@@ -181,4 +211,5 @@
 	</div>
 
 
-<?php include_once("footer.php"); ?>
+</body>
+</html>
